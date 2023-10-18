480007aede2fccd9a96dcfa2d8d90788be93a44c
javiech1 <92743651+javiech1@users.noreply.github.com>
2023-10-18 08:35:05 +0000

    add: has diffuse returns true phong.h

--- RTACG_Students 2/src/materials/phong.h
+++ RTACG_Students 2/src/materials/phong.h
@@ -3,23 +3,22 @@
 
 #include "material.h"
 
-class Phong: public Material
+class Phong : public Material
 {
 public:
     Phong();
-    Phong(Vector3D kd_,    Vector3D ks_,double shininess_);
+    Phong(Vector3D kd_, Vector3D ks_, double shininess_);
 
     Vector3D getReflectance(const Vector3D &n, const Vector3D &wo,
-                                    const Vector3D &wi) const;
+                            const Vector3D &wi) const;
     bool hasSpecular() const { return false; };
     bool hasTransmission() const { return false; };
-    bool hasDiffuseOrGlossy()const { return false; };
+    bool hasDiffuseOrGlossy() const { return true; };
 
 private:
     Vector3D kd;
     Vector3D ks;
     double shininess;
 };
-
 
 #endif // MATERIAL
